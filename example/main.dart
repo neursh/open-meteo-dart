a344289834f88578bb15cbbad02cf10adc8a6193
Neurs <31346263+neursh@users.noreply.github.com>
2024-07-09 09:26:13 +0700

    Fix format

--- example/main.dart
+++ example/main.dart
@@ -2,9 +2,9 @@ import 'package:open_meteo/open_meteo.dart';
 
 void main() async {
   var weather = Weather(
-    latitude: 52.52,
-    longitude: 13.41,
-    temperature_unit: TemperatureUnit.celsius);
+      latitude: 52.52,
+      longitude: 13.41,
+      temperature_unit: TemperatureUnit.celsius);
   var hourly = [Hourly.temperature_2m];
   var result = await weather.raw_request(hourly: hourly);
   print(result);
