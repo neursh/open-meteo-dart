155f799bf91ba15263c9006dfc3d72626d3800cd
Neurs <31346263+neursh@users.noreply.github.com>
2024-08-06 12:32:57 +0700

    Fix naming

--- _generate_variables.js
+++ _generate_variables.js
@@ -12,36 +12,36 @@ function consoleVariable(title, variables) {
 }
 
 function collectVariables() {
-  const colasped_docs = document.getElementsByClassName("accordion-button");
-  for (let i = 0; i < colasped_docs.length; i++) {
-    colasped_docs[i].click();
+  const colaspedDocs = document.getElementsByClassName("accordion-button");
+  for (let i = 0; i < colaspedDocs.length; i++) {
+    colaspedDocs[i].click();
   }
 
-  const hourly_variables = new Set();
-  const daily_variables = new Set();
-  const current_variables = new Set();
+  const hourlyVariables = new Set();
+  const dailyVariables = new Set();
+  const currentVariables = new Set();
 
   setTimeout(() => {
-    const form_docs = document.getElementsByClassName("form-check-input");
-    for (let i = 0; i < form_docs.length; i++) {
-      if (form_docs[i].getAttribute("name") === "hourly") {
-        hourly_variables.add(form_docs[i].getAttribute("value"));
+    const formDocs = document.getElementsByClassName("form-check-input");
+    for (let i = 0; i < formDocs.length; i++) {
+      if (formDocs[i].getAttribute("name") === "hourly") {
+        hourlyVariables.add(formDocs[i].getAttribute("value"));
       }
-      if (form_docs[i].getAttribute("name") === "daily") {
-        daily_variables.add(form_docs[i].getAttribute("value"));
+      if (formDocs[i].getAttribute("name") === "daily") {
+        dailyVariables.add(formDocs[i].getAttribute("value"));
       }
-      if (form_docs[i].getAttribute("name") === "current") {
-        current_variables.add(form_docs[i].getAttribute("value"));
+      if (formDocs[i].getAttribute("name") === "current") {
+        currentVariables.add(formDocs[i].getAttribute("value"));
       }
     }
 
-    consoleVariable("Hourly", hourly_variables);
-    consoleVariable("Daily", daily_variables);
-    consoleVariable("Current", current_variables);
+    consoleVariable("Hourly", hourlyVariables);
+    consoleVariable("Daily", dailyVariables);
+    consoleVariable("Current", currentVariables);
   }, 1000);
 }
 
-const doc_pages = [
+const docPages = [
   "/en/docs",
   "/en/docs/historical-weather-api",
   "/en/docs/ensemble-api",
@@ -51,9 +51,9 @@ const doc_pages = [
   "/en/docs/flood-api"
 ];
 
-for (let page = 0; page < doc_pages.length; page++) {
+for (let page = 0; page < docPages.length; page++) {
   setTimeout(() => {
-    const button = document.querySelector(`a[href="${doc_pages[page]}"]`);
+    const button = document.querySelector(`a[href="${docPages[page]}"]`);
     button.click();
     console.log(`%c${button.innerText}`, "color: green; font-size: 20px");
     setTimeout(() => {
