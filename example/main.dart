1c60753e7340af385c3bb2979ae2b1e98d1efc2e
Neurs <31346263+neursh@users.noreply.github.com>
2024-08-08 14:20:01 +0700

    Update example and README

--- example/main.dart
+++ example/main.dart
@@ -1,13 +1,14 @@
 import 'package:open_meteo/open_meteo.dart';
 
 void main() async {
-  final api = WeatherApi();
-  final response = await api.request(
+  final weather = WeatherApi();
+  final response = await weather.request(
     latitude: 52.52,
     longitude: 13.41,
-    current: {WeatherCurrent.temperature_2m},
+    hourly: {WeatherHourly.temperature_2m},
   );
-  final data = response.currentData[WeatherCurrent.temperature_2m]!;
-  final currentTemperature = data.value;
+  final data = response.hourlyData[WeatherHourly.temperature_2m]!;
+  final currentTemperature = data.values;
+
   print(currentTemperature);
 }
