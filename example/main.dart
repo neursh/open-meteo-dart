2ce24138e8a19ae86287df448370e03662d874c4
neursh <31346263+neursh@users.noreply.github.com>
2025-10-06 10:31:44 +0700

    Easier example

--- example/main.dart
+++ example/main.dart
@@ -1,26 +1,23 @@
 import 'package:open_meteo/open_meteo.dart';
 
 void main() async {
-  final weather = WeatherApi();
+  final weather = WeatherApi(
+    userAgent: "My-Flutter-App",
+    temperatureUnit: TemperatureUnit.celsius,
+  );
   final response = await weather.request(
-      models: {
-        OpenMeteoModel.best_match,
-        OpenMeteoModel.ecmwf_ifs025,
-      },
-      locations: {
-        OpenMeteoLocation(latitude: 16.16667, longitude: 107.83333),
-        OpenMeteoLocation(latitude: 52.52, longitude: 13.41),
-      },
-      hourly: {
-        WeatherHourly.temperature_2m
-      },
-      overrideUri: (url) {
-        print(url);
-        return url;
-      });
+    locations: {
+      OpenMeteoLocation(
+        latitude: 16.16667,
+        longitude: 107.83333,
+        startDate: DateTime(2025, 10, 1),
+        endDate: DateTime(2025, 10, 6),
+      )
+    },
+    hourly: {WeatherHourly.temperature_2m},
+  );
+  final data = response.segments[0].hourlyData[WeatherHourly.temperature_2m]!;
+  final temperatures = data.values;
 
-  for (final segment in response.segments) {
-    print(
-        '${segment.longitude},${segment.latitude} : ${segment.model} : ${segment.hourlyData[WeatherHourly.temperature_2m]!.values.length}');
-  }
+  print(temperatures);
 }
